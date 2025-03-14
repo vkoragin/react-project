53ae269256decc221d42bf3f8fed8c2439d925c3
Victor Koriagin <superiaschik@rambler.ru>
2025-03-14 16:15:06 +0300

    hw-4 fix review

--- src/components/review-form/review-form.jsx
+++ src/components/review-form/review-form.jsx
@@ -1,34 +1,18 @@
-import { useReducer } from 'react';
 import { Counter } from '../counter/counter';
 import { Button } from '../Button/button';
-import { MAX, MIN, DEFAULT_FORM_VALUE } from './consts';
-import {
-  SET_NAME_ACTION,
-  SET_TEXT_ACTION,
-  SET_RATING_ACTION,
-  CLEAR_FORM_ACTION,
-} from './actions';
-import { reducer } from './reducer';
+import { useReviewForm } from './use-review-form';
 
 export const ReviewForm = () => {
-  const [{ name, text, rating }, dispatch] = useReducer(
-    reducer,
-    DEFAULT_FORM_VALUE
-  );
-
-  const setName = (name) => dispatch({ type: SET_NAME_ACTION, payload: name });
-  const setText = (text) => dispatch({ type: SET_TEXT_ACTION, payload: text });
-  const incrementRating = () => {
-    if (rating < MAX) {
-      dispatch({ type: SET_RATING_ACTION, payload: rating + 1 });
-    }
-  };
-  const decrementRating = () => {
-    if (rating > MIN) {
-      dispatch({ type: SET_RATING_ACTION, payload: rating - 1 });
-    }
-  };
-  const clearForm = () => dispatch({ type: CLEAR_FORM_ACTION });
+  const {
+    name,
+    text,
+    rating,
+    setName,
+    setText,
+    incrementRating,
+    decrementRating,
+    clearForm,
+  } = useReviewForm();
 
   return (
     <form>
