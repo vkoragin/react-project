75673881961ab2414191bfcaa58ce1f18a0d764a
Victor Koriagin <superiaschik@rambler.ru>
2025-03-29 21:05:10 +0300

    hw-8

--- src/redux/entities/cart/slice.js
+++ src/redux/entities/cart/slice.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createSelector } from '@reduxjs/toolkit';
 
 export const cartSlice = createSlice({
   name: 'cartSlice',
@@ -20,17 +20,20 @@ export const cartSlice = createSlice({
     },
   },
   selectors: {
-    selectCartItems: (state) =>
-      Object.keys(state).reduce((acc, id) => {
-        acc.push({ id, amount: state[id] });
-
-        return acc;
-      }, []),
     selectAmountByRestaurantId: (state, id) => state[id],
   },
 });
 
-export const { selectCartItems, selectAmountByRestaurantId } =
-  cartSlice.selectors;
+export const { selectAmountByRestaurantId } = cartSlice.selectors;
 
 export const { addToCart, removeFromCart } = cartSlice.actions;
+
+const selectCartSlice = (state) => state.cartSlice;
+
+export const selectCartItems = createSelector([selectCartSlice], (cart) =>
+  Object.keys(cart).reduce((acc, id) => {
+    acc.push({ id, amount: cart[id] });
+
+    return acc;
+  }, [])
+);
