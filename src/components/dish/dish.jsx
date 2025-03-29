3a730bf2aec03360f853263f0785c04b5d23f958
Victor Koriagin <superiaschik@rambler.ru>
2025-03-29 11:31:36 +0300

    hw-7 fix

--- src/components/dish/dish.jsx
+++ src/components/dish/dish.jsx
@@ -3,11 +3,11 @@ import { useSelector } from 'react-redux';
 import { Counter } from '../counter/counter';
 import { useCount } from '../counter/use-count';
 import { UserContext } from '../user-context';
-import { selectDisheById } from '../../redux/entities/menu/slice';
+import { selectDishById } from '../../redux/entities/menu/slice';
 import styles from './dish.module.css';
 
 export const Dish = ({ id }) => {
-  const dish = useSelector((state) => selectDisheById(state, id));
+  const dish = useSelector((state) => selectDishById(state, id));
   const { name } = dish;
   const { count, increment, decrement } = useCount(id);
   const { user } = use(UserContext);
