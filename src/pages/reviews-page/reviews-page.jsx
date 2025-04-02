17b6b044e89537f913ff79895d59d409797c3bba
Victor Koriagin <superiaschik@rambler.ru>
2025-04-02 17:36:05 +0300

    hw-9 get reviews

--- src/pages/reviews-page/reviews-page.jsx
+++ src/pages/reviews-page/reviews-page.jsx
@@ -1,18 +1,31 @@
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router';
-import { selectRestaurantById } from '../../redux/entities/restaurants/slice';
 import { Reviews } from '../../components/reviews/reviews';
+import { useRequest } from '../../redux/hooks/use-request';
+import { getReviews } from '../../redux/entities/reviews/get-reviews';
+import { selectReviewsIds } from '../../redux/entities/reviews/slice';
 
 export const ReviewsPage = () => {
   const { restaurantId } = useParams();
-  const { reviews } = useSelector((state) =>
-    selectRestaurantById(state, restaurantId)
-  );
+  const requestStatus = useRequest(getReviews, restaurantId);
+  const reviews = useSelector(selectReviewsIds);
+
+  if (requestStatus === 'idle' || requestStatus === 'pending') {
+    return 'loading...';
+  }
+
+  if (requestStatus === 'rejected') {
+    return 'error';
+  }
 
   return (
     <>
-      <h3>Отзывы</h3>
-      <Reviews reviews={reviews} />
+      {!!reviews.length && (
+        <>
+          <h3>Отзывы</h3>
+          <Reviews reviews={reviews} />
+        </>
+      )}
     </>
   );
 };
