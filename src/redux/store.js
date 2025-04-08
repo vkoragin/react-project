0c413e016fd826c86905b6a922baa2e044f161d2
Victor Koriagin <superiaschik@rambler.ru>
2025-04-08 19:46:13 +0300

    hw-10 удалил лишние слайсы

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,20 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { restaurantsSlice } from './entities/restaurants/slice';
 import { cartSlice } from './entities/cart/slice';
-import { menuSlice } from './entities/menu/slice';
-import { reviewsSlice } from './entities/reviews/slice';
-import { usersSlice } from './entities/users/slice';
-import { requestSlice } from './entities/request/slice';
 import { api } from './servicies/api';
 
 export const store = configureStore({
   reducer: {
-    [restaurantsSlice.name]: restaurantsSlice.reducer,
     [cartSlice.name]: cartSlice.reducer,
-    [menuSlice.name]: menuSlice.reducer,
-    [reviewsSlice.name]: reviewsSlice.reducer,
-    [usersSlice.name]: usersSlice.reducer,
-    [requestSlice.name]: requestSlice.reducer,
     [api.reducerPath]: api.reducer,
   },
   middleware: (getDefaultMiddlewares) =>
