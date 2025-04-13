44c4a5ca237045bbc75d510e3a82d1e118d8c014
Victor Koriagin <superiaschik@rambler.ru>
2025-04-13 19:51:02 +0300

    hw-12 loaders and generateStaticParams

--- src/app/restaurants/[restaurantId]/page.js
+++ src/app/restaurants/[restaurantId]/page.js
@@ -1,5 +1,12 @@
 import { redirect } from 'next/navigation';
 
+export const generateStaticParams = async () => {
+  return [
+    { restaurantId: 'bb8afbec-2fec-491f-93e9-7f13950dd80b' },
+    { restaurantId: 'd9241927-09e1-44f3-8986-a76346869037' },
+  ];
+};
+
 const RestaurantPageWrapper = async ({ params }) => {
   const { restaurantId } = await params;
 
